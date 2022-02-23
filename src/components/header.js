663efd91557c5987248be5a85374a286b8c311cf
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-23 20:15:24 +0100

    Added header Drawer for small screens

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -20,50 +21,79 @@ const sections = [
 ];
 
 const Header = () => {
+  const [drawer, setDrawer] = useState(false);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" elevation={0} sx={{bgcolor: '#2b8273'}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ display: {md: 'none'}, mr: 2 }}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed" elevation={0} sx={{bgcolor: '#2b8273'}}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: {md: 'none'}, mr: 2 }}
+              onClick={() => setDrawer(!drawer)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Paul Chaperon
+            </Typography>
+          </Toolbar>
+          <Stack
+            display={{ xs: 'none', md: 'flex' }}
+            direction="row"
+            justifyContent="space-around"
+            color="#353535"
+            bgcolor="white"
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Paul Chaperon
-          </Typography>
-        </Toolbar>
-        <Stack
-          display={{ xs: 'none', md: 'flex' }}
-          direction="row"
-          justifyContent="space-around"
-          color="#353535"
-          bgcolor="white"
-        >
-          {
-            sections.map(({text}) => 
-              <Button
-                key={text}
-                color="inherit"
-                sx={{
-                  px: 2,
-                  py: 1.5,
-                  borderRadius: 0,
-                  fontSize: '0.7em',
-                  letterSpacing: 2,
-                }}
-              >
-                {text}
-              </Button>
-            )
-          }
-        </Stack>
-      </AppBar>
-    </Box>
+            {
+              sections.map(({text}) => 
+                <Button
+                  key={text}
+                  color="inherit"
+                  sx={{
+                    px: 2,
+                    py: 1.5,
+                    borderRadius: 0,
+                    fontSize: '0.7em',
+                    letterSpacing: 2,
+                  }}
+                >
+                  {text}
+                </Button>
+              )
+            }
+          </Stack>
+        </AppBar>
+      </Box>
+      <Drawer
+        anchor="left"
+        open={drawer}
+        onClose={() => setDrawer(false)}
+        sx={{ display: {md: 'none'} }}
+      >
+        {
+          sections.map(({text}, i) => 
+            <Button
+              key={text}
+              color="inherit"
+              sx={{
+                mt: i?0.8:5,
+                px: 5,
+                py: 1.5,
+                borderRadius: 0,
+                letterSpacing: 2,
+              }}
+            >
+              {text}
+            </Button>
+          )
+        }
+      </Drawer>
+    </>
   );
 }
 
