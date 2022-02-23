ce0e0c0b11faaf35931371e64de016a072a13d4b
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-24 00:28:09 +0100

    Fixed headings size on small screens

---
+++ src/components/metadata.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {Helmet} from "react-helmet";
+
+const Metadata = ({}) => {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Développeur indépendant | Paul Chaperon</title>
+    </Helmet>
+  );
+}
+
+export default Metadata;
