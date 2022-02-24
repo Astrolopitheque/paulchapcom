75dabc9125c8627d8a048315690c0cc4b6768435
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-25 00:00:50 +0100

    Who am I section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,75 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import Metadata from '../components/metadata';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-const StyledHeading = ({ children }) => {
+const StyledHeading = ({ children, align, color, sx }) => {
   return (
     <Typography
       variant="h2"
-      fontSize={{xs: '1.6em', md: '3.5em'}}
+      fontSize={{xs: '1.9em', md: '3.5em'}}
+      align={align}
+      sx={{
+        mb: 3,
+        color,
+        ...sx,
+      }}
     >
-      {'> > > '}{children}
+      {(align == 'left' || align == 'justify') && '> > > '}
+      {align == 'center' && '~~ '}
+      {children}
+      {align == 'center' && ' ~~'}
+      {align == 'right' && ' • • •'}
     </Typography>
   );
 }
 
-const Home = () => {
+StyledHeading.defaultProps = {
+  align: 'left',
+}
+
+const Index = () => {
   return (
     <Layout>
       <Metadata />
       <Container>
-        <StyledHeading>Qui suis-je ?</StyledHeading>
-        <StyledHeading>Services proposés</StyledHeading>
+        <Box display="flex" justifyContent="center">
+        <Paper
+          sx={{
+            color: 'white',
+            bgcolor: '#7dabaa',
+            width: {md: '50%'},
+            borderRadius: 0,
+            p: 3,
+            mb: 2,
+            textAlign: 'center'
+          }}
+        >
+          <StyledHeading align="center">Qui suis-je ?</StyledHeading>
+          <Box display="flex" flexDirection="column" alignItems={'flex-end'}>
+            <Typography mb={2} align="justify" width={{md: '90%'}}>
+              <b>Passionné d'informatique</b> depuis mon enfance,
+              j'ai longuement exploré cette branche des sciences à travers <b>différents projets</b>.
+              Aujourd'hui âgé de 18 ans et étudiant à Sorbonne Université,
+              j'ai décidé de me lancer dans le monde professionnel en tant que <b>développeur indépendant</b>.
+            </Typography>
+            <Typography variant="h6" component="p">
+              Paul Chaperon
+            </Typography>
+          </Box>
+        </Paper>
+        </Box>
+        <StyledHeading align="right">Services proposés</StyledHeading>
         <StyledHeading>Mes compétences</StyledHeading>
-        <StyledHeading>Mon portfolio</StyledHeading>
-        <StyledHeading>Mes compétences</StyledHeading>
+        <StyledHeading align="right">Mon portfolio</StyledHeading>
         <StyledHeading>Tarifs</StyledHeading>
-        <StyledHeading>Contactez-moi !</StyledHeading>
+        <StyledHeading align="center">Contactez-moi !</StyledHeading>
       </Container>
     </Layout>
   );
 }
 
-export default Home;
+export default Index;
