816ef2a8e6652f19303b30c1f0b4a11438fcc24e
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-25 23:55:30 +0100

    Ableness components animation restart when enter screen

--- src/components/circularcompetence.js
+++ src/components/circularcompetence.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import TrackVisibility from 'react-on-screen';
 
 const CircularCompetence = ({ children, ableness, size }) => {
   let color = 'blue';
@@ -9,29 +10,28 @@ const CircularCompetence = ({ children, ableness, size }) => {
   else if(ableness < 75) color = 'yellow';
   else if(ableness < 100) color = 'green';
 
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    setProgress(ableness);
-  }, [ableness]);
-
   return (
-    <Box
-      p={1}
-      bgcolor="#c5ccd7"
-      borderRadius="50%"
-      display="inline-flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <CircularProgress
-        variant="determinate"
-        value={progress}
-        size={size}
-        sx={{ color }}
-      />
-      <span style={{ position: 'absolute' }}>{children}</span>
-    </Box>
+    <TrackVisibility tag="span">
+      {
+        ({isVisible}) =>
+          <Box
+            p={1}
+            bgcolor="#c5ccd7"
+            borderRadius="50%"
+            display="inline-flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CircularProgress
+              variant="determinate"
+              value={isVisible ? ableness : 0}
+              size={size}
+              sx={{ color }}
+            />
+            <span style={{ position: 'absolute' }}>{children}</span>
+          </Box>
+      }
+    </TrackVisibility>
   );
 }
 
