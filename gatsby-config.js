1bb89bd5e10dad1bba208f97c5fd458a2b1499ac
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-27 04:22:59 +0100

    Added page description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
     siteMetadata: {
       title: `PaulChap.com`,
-        siteUrl: `https://www.yourdomain.tld`,
+      siteUrl: `https://www.paulchap.com`,
     },
     plugins: [
-
+      {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+          icon: 'src/images/favicon.png',
+        },
+      },
     ]
 }
