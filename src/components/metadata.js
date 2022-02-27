cbb0259c1ce70df5b65480a7a170baf622819b1e
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-02-27 03:26:16 +0100

    Added page description

--- src/components/metadata.js
+++ src/components/metadata.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {Helmet} from "react-helmet";
 
+const description = `Étudiant à Sorbonne Université et développeur indépendant, je me définis comme un passionné d'informatique. Les services que je propose : développement, design, création numérique.`;
+
 const Metadata = () => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
       <title>Développeur indépendant | Paul Chaperon</title>
+      <meta name="description" content={description} />
     </Helmet>
   );
 }
