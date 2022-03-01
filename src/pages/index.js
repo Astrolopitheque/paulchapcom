72d1b91e8d514147e297707ab08e9b447116e82f
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-03-01 02:34:13 +0100

    Changed prices table

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,15 +93,19 @@ const Index = () => {
           text='PHP'
         />
         <StyledHeading id='portfolio' align='right'>Mon portfolio</StyledHeading>
-        <StyledHeading id='prices'>Tarifs</StyledHeading>
         TODO : rendre le tableau bien drippy issou + le remplir coijdsgvlsiudwo
         <table style={{ border: '1px solid #333' }}>
           <thead>
             <tr>
-              <th colspan='2' style={{ backgroundColor: '#333', color: '#fff' }}>Agneugneu devis</th>
+              <th colspan={2} style={{ backgroundColor: '#333', color: '#fff' }}>
+                <StyledHeading id='prices'>Tarifs</StyledHeading>
+              </th>
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td colspan={2} style={{ border: '1px solid #333' }}>Faut un devis bozo</td>
+            </tr>
             <tr>
               <td style={{ border: '1px solid #333' }}>Tarif horaire</td>
               <td style={{ border: '1px solid #333' }}>Tarif journalier moyen</td>
