8df2931d90d043f5194f6545459f301a19f98a50
Astrolopitheque <40475109+Astrolopitheque@users.noreply.github.com>
2022-08-08 00:30:21 +0200

    Centralized theme and changed colors

---
+++ src/variables.js
@@ -0,0 +1,12 @@
+export const theme = {
+  primary: '#0a2c4d',
+  secondary: '#d16e17',
+  light: {
+    secondary: '#f5c178',
+  },
+  text: {
+    light: '#fff',
+    dark: '#353535',
+    disabled: '#777',
+  },
+};
